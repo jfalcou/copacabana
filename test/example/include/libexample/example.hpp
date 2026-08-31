852c3764e927a97ce2fd70643619a521983bb843
Joel Falcou <joel.falcou@lri.fr>
2026-08-31 17:46:37 +0200

    Fix #48 - Move the example under test, where it has always belonged

---
+++ test/example/include/libexample/example.hpp
@@ -0,0 +1,33 @@
+//======================================================================================================================
+//  Copacabana - Common CMake Package Tools
+//  Copyright : Copacabana Project Contributors
+//  SPDX-License-Identifier: BSL-1.0
+//======================================================================================================================
+#ifndef EXAMPLE_HPP_INCLUDED
+#define EXAMPLE_HPP_INCLUDED
+namespace example
+{
+  template<typename T>
+  inline constexpr auto value = T(1337.42);
+}
+#include <vector>
+namespace example
+{
+  template<typename T>
+  struct type
+  {
+    std::vector<T> data;
+  };
+}
+#include <string>
+namespace example
+{
+  inline const std::string version = "1.0.1";
+  template<typename T>
+  constexpr T clamp_to_value(T x)
+  {
+    if(x > value<T>) return value<T>;
+    return x;
+  }
+}
+#endif
